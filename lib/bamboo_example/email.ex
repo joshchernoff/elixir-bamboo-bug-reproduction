7eed195bdbeaf360aaf68777584d9336016ea49e
Tiago Duarte <tmpduarte@gmail.com>
2020-05-07 16:03:57 +0100

    example of bamboo not working as per the documentation

--- lib/bamboo_example/email.ex
+++ lib/bamboo_example/email.ex
@@ -1,13 +1,13 @@
 defmodule BambooExample.Email do
+  use Bamboo.Phoenix, view: BambooExampleWeb.LayoutView
   import Bamboo.Email
 
+  # as per https://hexdocs.pm/bamboo/Bamboo.Phoenix.html
   def welcome_email do
-    new_email(
-      to: "humans@coletiv.com",
-      from: "tiago@myapp.com",
-      subject: "Bamboo Test",
-      html_body: "<strong>Thanks for joining!</strong>",
-      text_body: "Thanks for joining!"
-    )
+    new_email()
+    |> put_text_layout({BambooExampleWeb.LayoutView, "email.text"})
+    |> put_html_layout({BambooExampleWeb.LayoutView, "email.html"})
+    # Pass atom to render html AND plain text templates
+    |> render(:welcome)
   end
 end

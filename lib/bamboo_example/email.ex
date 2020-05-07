e6a9add81ee7543d92396a30cdff73567280a1a8
Josh Chernoff <jchernoff@morphic.pro>
2020-05-07 13:00:36 -0700

    fix #3

--- lib/bamboo_example/email.ex
+++ lib/bamboo_example/email.ex
@@ -5,6 +5,7 @@ defmodule BambooExample.Email do
   # as per https://hexdocs.pm/bamboo/Bamboo.Phoenix.html
   def welcome_email do
     new_email()
+    |> from({"Some one random", "no-reply@exmaple.com"})
     |> put_text_layout({BambooExampleWeb.LayoutView, "email.text"})
     |> put_html_layout({BambooExampleWeb.LayoutView, "email.html"})
     # Pass atom to render html AND plain text templates
